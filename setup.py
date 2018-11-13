c7f42dd8bf6065547c5aa204073d8d1a648b48fb
Sam Mulube <sam@umbrellium.co.uk>
2018-11-13 13:34:37 +0000

    Add first pass at a setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="zenroom",
+    version="0.0.1",
+    author="Sam Mulube",
+    author_email="sam@thingful.net",
+    description="Python wrapper for the Zenroom virtual machine",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/thingful/zenroom-py",
+    packages=setuptools.find_packages(),
+)
