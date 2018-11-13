49c2557eece0eb22b34fc300488493fab5c1a45d
Sam Mulube <sam@umbrellium.co.uk>
2018-11-13 00:22:25 +0000

    Figure basic method for caling zenroom from python 3
    Commiting quickly before it stops again...

--- zenroom/zenroom.py
+++ zenroom/zenroom.py
@@ -1,4 +1,42 @@
-from . import _zenroom
+import ctypes
+import os.path
 
-def exec(script, keys=None, data=None, conf=None, verbosity=1):
-    _zenroom.zenroom_exec(script, conf, keys, data, verbosity)
+zenroom_path = os.path.join(
+        os.path.dirname(__file__), "_zenroom.so")
+
+_zenroom = ctypes.CDLL(zenroom_path)
+
+# Module variable - used to set the max size of the buffer when handling
+# zenroom output
+__MAX_STRING__ = 4096
+
+def execute(script, keys=None, data=None, conf=None, verbosity=1):
+    """Invoke Zenroom, capturing and returning the output as a byte string
+
+    This function is the primary method we expose from this wrapper library,
+    which attempts to make Zenroom slightly simpler to call from Python. This
+    wrapper has only been developed for a specific pilot project within DECODE,
+    so beware - the code within this wrapper may be doing very bad things that
+    the underlying Zenroom tool does not require.
+
+    Args:
+        script (str): Required string containing script which Zenroom will execute
+        keys (str): Optional string containing keys which Zenroom will use
+        data (str): Optional string containing data upon which Zenroom will operate
+        conf (str): Optional string containing conf data for Zenroom
+        verbosity (int): Optional int which controls Zenroom's log verbosity ranging from 1 (least verbose) up to 3 (most verbose)
+
+    Returns:
+            bytes: The output from Zenroom expressed as a byte string
+    """
+    stdout_buf = ctypes.create_string_buffer(b"\000", __MAX_STRING__)
+    stdout_len = ctypes.c_size_t(__MAX_STRING__)
+    stderr_buf = ctypes.create_string_buffer(b"\000", __MAX_STRING__)
+    stderr_len = ctypes.c_size_t(__MAX_STRING__)
+
+    _zenroom.zenroom_exec_tobuf(
+            script, conf, keys, data, verbosity,
+            ctypes.byref(stdout_buf), stdout_len,
+            ctypes.byref(stderr_buf), stderr_len)
+
+    return stdout_buf.value
