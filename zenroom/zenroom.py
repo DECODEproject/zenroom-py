3498d959036d27499a5ea3d2992b9170933ed049
Sam Mulube <sam@umbrellium.co.uk>
2018-11-21 10:13:02 +0000

    Non-functional addition of some basic error handling.
    Currently this doesn't seem to work with the build of Zenroom included
    here, as passing an erorring script into Zenroom seems to immediately
    kill the Python process.

--- zenroom/zenroom.py
+++ zenroom/zenroom.py
@@ -1,14 +1,20 @@
 import ctypes
 import os.path
 
-zenroom_path = os.path.join(
-        os.path.dirname(__file__), "_zenroom.so")
+zenroom_path = os.path.join(os.path.dirname(__file__), "_zenroom.so")
 
 _zenroom = ctypes.CDLL(zenroom_path)
 
 # Module variable - used to set the max size of the buffer when handling
 # zenroom output
 __MAX_STRING__ = 4096
+
+
+class Error(Exception):
+    """Base class for Zenroom errors."""
+
+    pass
+
 
 def execute(script, keys=None, data=None, conf=None, verbosity=1):
     """Invoke Zenroom, capturing and returning the output as a byte string
@@ -34,9 +40,19 @@ def execute(script, keys=None, data=None, conf=None, verbosity=1):
     stderr_buf = ctypes.create_string_buffer(b"\000", __MAX_STRING__)
     stderr_len = ctypes.c_size_t(__MAX_STRING__)
 
-    _zenroom.zenroom_exec_tobuf(
-            script, conf, keys, data, verbosity,
-            ctypes.byref(stdout_buf), stdout_len,
-            ctypes.byref(stderr_buf), stderr_len)
+    err = _zenroom.zenroom_exec_tobuf(
+        script,
+        conf,
+        keys,
+        data,
+        verbosity,
+        ctypes.byref(stdout_buf),
+        stdout_len,
+        ctypes.byref(stderr_buf),
+        stderr_len,
+    )
+
+    if err != 0:
+        raise Error(stderr_buf.value)
 
     return stdout_buf.value
