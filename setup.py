2fe62494484e7eee89dfb3dd10871c284ad4f0e0
Puria Nafisi Azizi <puria@dyne.org>
2019-03-03 08:05:27 +0100

    :bookmark: Preparing for 0.1.2

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ package_data = [
 
 setuptools.setup(
     name="zenroom",
-    version="0.1.1",
+    version="0.1.2",
     author="Sam Mulube",
     author_email="sam@thingful.net",
     description="Python wrapper for the Zenroom virtual machine",
