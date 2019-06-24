fc669970385b412b053add08599a4f91fefcd043
Puria Nafisi Azizi <puria@dyne.org>
2019-06-24 09:12:36 +0200

    :bookmark: Adding `publish' to make easy releases

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 import setuptools
 
+VERSION="1.0.3"
+
+if sys.argv[-1] == 'publish':
+    os.system(f"git tag -a {VERSION}")
+    os.system('python setup.py sdist bdist_wheel')
+    os.system('twine upload dist/*')
+    sys.exit()
+
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="zenroom",
-    version="1.0.3",
+    version=VERSION,
     author="Sam Mulube",
     author_email="sam@thingful.net",
     maintainer="Puria Nafisi Azizi",
