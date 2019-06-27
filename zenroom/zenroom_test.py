7f9b3dbe0ca6c8486efde1da08a6454eb88b534a
Puria Nafisi Azizi <puria@dyne.org>
2019-06-27 10:04:29 +0200

    📦 new zenroom build and random test unit

--- zenroom/zenroom_test.py
+++ zenroom/zenroom_test.py
@@ -5,7 +5,7 @@ import pytest
 from zenroom import zenroom
 from zenroom.zenroom import ZenroomException
 
-LOAD_FATIGUE = 100
+LOAD_FATIGUE = 20
 
 
 def test_basic():
@@ -69,6 +69,30 @@ def test_broken_zencode():
     assert "{}" == result
 
 
+def test_create_petition_with_rng():
+    script = """Scenario 'coconut': "Citizen creates a new petition, using his own key, the credential and the credential issuer's public"
+Given that I am known as 'identifier'
+and I have my keypair
+and I have a signed credential
+and I use the verification key by 'issuer_identifier'
+When I aggregate all the verification keys
+and I generate a credential proof
+and I create a new petition 'petition'
+Then print all data
+    """
+    keys = '{"identifier":{"schema":"cred_keypair","zenroom":"0.9","encoding":"hex","public":"04356878b230d0bffc7e913736dbc2cdcfecfc21f02195177c1d501513a1c10fafc534668a306db5a7dcd57df8242226d61920f766bc0dca690598bdcd7aae09490d7681f9ec6197d84c8ed9e5484a8ad21717d7a224697fd3649279089970c71d","private":"aeea220f7c4d3d39b58acc20c84553f6b2864fde01ff1373018e5197074302b3","curve":"bls383"},"credential":{"h":"04424954e09f573023b34e5c764ee3ea07b0942cadc9f738ba057afed1a52eef78edd3172d58c4cdfcfbd393686a0b11fe25662dd4e632752e26814236699cde98b4c04139fe73a63414cb1775c72654e2024559a1b46ea164f41a2e53f9bdb90f","zenroom":"0.9","curve":"bls383","schema":"aggsigma","encoding":"hex","s":"042c52807efc2506e16ec1f0db6002d5cf6d55b16e9645be9ba3a557a7bde9f2302272014f63591bc05a3bae35a7462a4b3ef2ef956e34210cdde9be9286ad9571d2c18c8ebd175924c42270e425391e4f279c5d498ae9f97f06d40f4bf6cd21a1"}}'
+    data = '{"issuer_identifier":{"verify":{"beta":"0d619263c6048d0ab94f825f0f9f30d5f441df3d78e6c34e64eb3d6a2d3b764d51f7722dc18456b986a8291a525985e747841ebd9788b1c505ca5584d752a3b9e5d294666745cf14b0df49fd81dd35ffe16f752fcd8dc9a4f69579528cdebf932272a28dc8c9c7d70af121e8061b8dc0867f0540e2b05306344c987d65c63d63179bf5d64ea7c6211a4e65bebc5b4eae4e649e46eb70094c5ae8fb271dfccce5d56ef7385ebb46c7e538a07fa8e4afbdeba03c71cc882a72a9042f8db2374a6b","alpha":"12835db70ccae8be2b58e6b73068368871189b19e2602dbfbf05bcbe7702d9b92916b3ecbaff93c6c560f48bc13cdc80469275f1adac04b5e568cda5a6c56651a2f8976dd947e9c72910432b495e6a605b99f97c5f3b955979e1668cac3f4ca13739b6e7a1866f7da679e8764d6f2ff54450f74b2033d1f2ae8cd1f32379df9242c255a773c0d44f23a5b666fcd20b0938e99c7b8f9d61bfa7982dcdd054d691266d3271fac6ea7491a8af4597dee7c1d409040a8014e1ad3c32370852ca4dc7"}}}'
+    results = []
+    for _ in range(LOAD_FATIGUE):
+        result, error = zenroom.zencode_exec_rng(script=script,
+                                                 data=data,
+                                                 keys=keys,
+                                                 random_seed=bytearray("random_se3d", "utf=8"))
+        sorted_result = json.dumps(json.loads(result), sort_keys=True)
+        results.append(sorted_result)
+    assert len(set(results)) == 1
+
+
 def test_random():
     script = """rng = RNG.new()
     buf = rng:octet(16)
@@ -76,23 +100,9 @@ def test_random():
     """
     results = []
     for _ in range(LOAD_FATIGUE):
-        result, error = zenroom.zenroom_exec_rng(script=script, random_seed=bytearray("", "utf=8"))
+        result, error = zenroom.zenroom_exec_rng(script=script, random_seed=bytearray("random_seed", "utf=8"))
         results.append(result)
-    assert len(set(results)) == len(results)
-
-
-def test_random_zencode():
-    script = """Scenario 'coconut': "test"
-    Given that I am known as 'identifier'
-    When I create my new keypair
-    Then print all data
-    """
-    results = []
-    for _ in range(LOAD_FATIGUE):
-        result, error = zenroom.zencode_exec_rng(script=script, random_seed=bytearray("", "utf=8"))
-        results.append(result)
-    assert len(set(results)) == len(results)
-
+    assert len(set(results)) == 1
 
 def test_load_test():
     contract = """Scenario 'coconut': "To run over the mobile wallet the first time and store the output as keypair.keys"
